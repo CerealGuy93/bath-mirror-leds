25eb67a2cab2c464bd69dc153c2a40b05343d51e
Philipp Wegner <info@philipp-wegner.net>
2023-04-10 22:09:16 +0200

    wupsi
    Brightness adjustment via settings call

--- src/config.h
+++ src/config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-const char* SSID = "FRITZ!Box 7590 BV";
-const char* WLAN_PASS = "31643015955394401988";
+const char* SSID = "<SSID>";
+const char* WLAN_PASS = "<WLAN PASS>";
 
 #endif
